cca93787ec79769ae6836d8b4433348dc19f8951
Manjunath Bhandari <bhandarimanjunath98@gmail.com>
2025-07-04 18:47:32 +0530

    Removed from strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ServiceProvider } from "./context/ServiceProvider.jsx";
+import App from "./App.jsx";
 import { AdminProvider } from "./context/adminProvider.jsx";
 import { BookingProvider } from "./context/BookingProvider.jsx";
+import { ServiceProvider } from "./context/ServiceProvider.jsx";
+import "./index.css";
 
-createRoot(
-  document.getElementById("root")
-).render(
+createRoot(document.getElementById("root")).render(
   <AdminProvider>
     <ServiceProvider>
       <BookingProvider>
         <BrowserRouter>
-          <StrictMode>
-            <App />
-          </StrictMode>
+          <App />
         </BrowserRouter>
       </BookingProvider>
     </ServiceProvider>
