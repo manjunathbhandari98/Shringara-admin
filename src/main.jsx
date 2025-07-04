69b2b3a4d0e10e4b9320dc0ddbc63b83e919f266
Manjunath Bhandari <bhandarimanjunath98@gmail.com>
2025-07-04 18:57:20 +0530

    added favicon icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { AdminProvider } from "./context/adminProvider.jsx";
+import { AdminProvider } from "./context/AdminProvider.jsx";
 import { BookingProvider } from "./context/BookingProvider.jsx";
 import { ServiceProvider } from "./context/ServiceProvider.jsx";
 import "./index.css";
